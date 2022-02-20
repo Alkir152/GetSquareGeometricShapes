f2c44045399bcd9a523330719ea77215d2903192
Aleksey <Aleksey@PC-ALEXEY>
2022-02-20 21:14:21 +0300

    Add project files.

---
+++ GetSquare/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace GetSquare
+{
+    public class Circle
+    {
+        double result;
+        public Circle(double rad)
+        {
+            if (rad <= 0)
+                result = 0;
+            else
+                result = Math.PI * Math.Pow(rad, 2);
+        }
+        public void GetSquare()
+        {
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Triangle
+    {
+        double result;
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            double sideAPow = Math.Pow(sideA, 2),
+                sideBPow = Math.Pow(sideB, 2),
+                sideCPow = Math.Pow(sideC, 2);// переменные для проверки на прямоугольный треугольник
+
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0 || sideA + sideB < sideC || sideC + sideB < sideA || sideA + sideC < sideB)
+                result = 0;
+            else if (sideAPow + sideBPow == sideCPow || sideCPow + sideBPow == sideAPow || sideAPow + sideCPow == sideBPow)
+            {
+                double[] array = { sideA, sideB, sideC };
+                double temp;
+                for (int i = 0; i < array.Length; i++)// отсортируем длины сторон в порядке убывания
+                {
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[i] > array[j])
+                        {
+                            temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+                result = (array[1] * array[2]) / 2;
+            }
+            else
+            {
+                double perimeter = (sideA + sideB + sideC) / 2;
+                result = Math.Sqrt(perimeter * (perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC));
+            }
+        }
+        public void GetSquare()
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ GetSquareGeometricShapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GetSquare;
+
+namespace GetSquareGeometricShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Радиус: ");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            Circle circle = new Circle(rad);
+            circle.GetSquare();
+
+            double[] array = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Введите длину стороны {i}: ");
+                array[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Triangle triangle = new Triangle(array[0], array[1], array[2]);
+            triangle.GetSquare();
+        }
+    }
+}
